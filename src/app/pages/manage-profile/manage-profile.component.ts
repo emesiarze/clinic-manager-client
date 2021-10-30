4d3dfe7d33431a48a870a2ef740aecb070d087a5
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-10-30 12:59:07 +0200

    Add manage-profile page

--- src/app/pages/manage-profile/manage-profile.component.ts
+++ src/app/pages/manage-profile/manage-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {User} from "../../models/user";
 
 @Component({
   selector: 'app-manage-profile',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-profile.component.scss']
 })
 export class ManageProfileComponent implements OnInit {
+  private _form: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder, private _authService: AuthService) { }
 
-  ngOnInit(): void {
+  get form(): FormGroup {
+    return this._form;
   }
 
+  private get user(): User {
+    return this._authService.user!;
+  }
+
+  ngOnInit(): void {
+    this._form = this.createForm();
+  }
+
+  private createForm(): FormGroup {
+    return this._fb.group({
+      login: { value: this.user.login || undefined, disabled: true },
+      fullName: this.user.fullName || undefined,
+    });
+  }
+
+  public updateUser(): void {
+    const user = {
+      id: this.user.id,
+      fullName: this._form.get('fullName')!.value,
+      login: this.form.get('login')!.value,
+      password: this.user.password,
+      isWorker: this.user.isWorker
+    };
+
+    console.log(user);
+  }
 }
