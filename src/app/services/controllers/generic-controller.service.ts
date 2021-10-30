99956defee556d3f832943383c074b333b48d3ec
Hubert Wawrzacz <hwawrzacz.dev@gmail.com>
2021-10-30 14:37:00 +0200

    Add generic controller and item service

---
+++ src/app/services/controllers/generic-controller.service.ts
@@ -0,0 +1,36 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpParams} from "@angular/common/http";
+import {CommonResponse} from "../../models/response";
+import {Observable} from "rxjs";
+import {Identifiable} from "../../models/Identifiable";
+
+@Injectable({
+  providedIn: 'root'
+})
+export abstract class GenericControllerService<T extends Identifiable> {
+  protected constructor(protected _endpointUrl: string, protected _http: HttpClient) { }
+
+  public getAllItems(): Observable<CommonResponse<T[]>> {
+    return this._http.get<CommonResponse<T[]>>(`/api/${this._endpointUrl}/all`);
+  }
+
+  public getItem(id: string): Observable<CommonResponse<T>> {
+    const params = new HttpParams().appendAll({ id: id });
+    return this._http.get<CommonResponse<T>>(`/api/${this._endpointUrl}`, { params: params});
+  }
+
+  public addItem(item: T): Observable<CommonResponse<string>> {
+    return this._http.post<CommonResponse<string>>(`/api/${this._endpointUrl}`, item);
+  }
+
+  public updateItem(item: T): Observable<CommonResponse<string>> {
+    const params = new HttpParams().appendAll({ id: item.id });
+    return this._http.put<CommonResponse<string>>(`/api/${this._endpointUrl}`, { params: params});
+  }
+
+  public deleteItem(id: string): Observable<CommonResponse<string>> {
+    const params = new HttpParams().appendAll({ id: id });
+    return this._http.get<CommonResponse<string>>(`/api/${this._endpointUrl}`, { params: params});
+  }
+
+}
---
+++ src/app/services/generic-item.service.ts
@@ -0,0 +1,100 @@
+import {Injectable} from '@angular/core';
+import {GenericControllerService} from "./controllers/generic-controller.service";
+import {SnackBarService} from "./snack-bar.service";
+import {Observable, of} from "rxjs";
+import {CommonResponse} from "../models/response";
+import {catchError, map} from "rxjs/operators";
+import {Identifiable} from "../models/Identifiable";
+
+enum Action {
+  Add = 'Dodano',
+  Delete = 'Usunieto',
+  Update = 'Zaktualizowano'
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GenericItemService<T extends Identifiable> {
+  constructor(protected controller: GenericControllerService<T>, protected snackBarService: SnackBarService) { }
+
+  public getAllItems(): Observable<T[] | null> {
+    return this.controller.getAllItems().pipe(
+      map((result: CommonResponse<T[]>) => {
+        const data = result.data;
+        !result.isSuccess ? this.handleResponseFailed(result.errorMessage) : {};
+        return data;
+      }),
+      catchError((err) => {
+        this.handleDefaultError(err);
+        return of(null);
+      })
+    );
+  }
+
+  public getItem(id: string): Observable<T | null> {
+    return this.controller.getItem(id).pipe(
+      map((result: CommonResponse<T>) => {
+        const data = result.data;
+        !result.isSuccess ? this.handleResponseFailed(result.errorMessage) : {};
+        return data;
+      }),
+      catchError((err) => {
+        this.handleDefaultError(err);
+        return of(null);
+      })
+    );
+  }
+
+  public addItem(item: T): Observable<boolean | null> {
+    return this.controller.addItem(item).pipe(
+      map((result: CommonResponse<string>) => {
+        result.isSuccess ? this.handleActionSuccess(Action.Add) : this.handleResponseFailed(result.errorMessage)
+        return result.isSuccess;
+      }),
+      catchError((err) => {
+        this.handleDefaultError(err);
+        return of(null);
+      })
+    );
+  }
+
+  public updateItem(item: T): Observable<boolean | null> {
+    return this.controller.updateItem(item).pipe(
+      map((result: CommonResponse<string>) => {
+        result.isSuccess ? this.handleActionSuccess(Action.Update) : this.handleResponseFailed(result.errorMessage);
+        return result.isSuccess;
+      }),
+      catchError((err) => {
+        this.handleDefaultError(err);
+        return of(null);
+      })
+    );
+  }
+
+  public deleteItem(id: string): Observable<boolean | null> {
+    return this.controller.deleteItem(id).pipe(
+      map((result: CommonResponse<string>) => {
+        result.isSuccess ? this.handleActionSuccess(Action.Delete) : this.handleResponseFailed(result.errorMessage);
+        return result.isSuccess;
+      }),
+      catchError((err) => {
+        this.handleDefaultError(err);
+        return of(null);
+      })
+    );
+  }
+
+  private handleActionSuccess(action: Action): void {
+    this.snackBarService.openSuccessSnackBar(`${action.toString()} element`);
+  }
+
+  private handleResponseFailed(error: string): void {
+    this.snackBarService.openErrorSnackBar('Podczas przetwarzania zapytania wystąpił błąd');
+    console.error(error);
+  }
+
+  private handleDefaultError(error: string): void {
+    this.snackBarService.openErrorSnackBar('Wystąpił błąd', error);
+  }
+}
